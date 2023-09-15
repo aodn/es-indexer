caaf8ff4c875976d7fb67d6e47eeb7edc72cf5f3
Viet Nguyen <vh.nguyen@utas.edu.au>
2023-09-15 17:31:38 +1000

    Update the boilerplate code

---
+++ src/main/java/au/org/aodn/esindexer/controller/IndexerController.java
@@ -0,0 +1,20 @@
+package au.org.aodn.esindexer.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+@Component
+public class IndexerController {
+
+}
