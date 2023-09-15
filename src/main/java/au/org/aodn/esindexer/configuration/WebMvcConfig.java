1c74478bf2f9b266d79b4c8c20edff2744eb5764
Viet Nguyen <vh.nguyen@utas.edu.au>
2023-09-15 17:24:56 +1000

    Init API boilerplate

---
+++ src/main/java/au/org/aodn/esindexer/configuration/WebMvcConfig.java
@@ -0,0 +1,26 @@
+package au.org.aodn.esindexer.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+@Configuration
+public class WebMvcConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        final RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(jacksonSupportedTypes());
+        return restTemplate;
+    }
+
+    private HttpMessageConverter jacksonSupportedTypes() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setSupportedMediaTypes(Arrays.asList(MediaType.parseMediaType("text/plain;charset=utf-8"), MediaType.APPLICATION_OCTET_STREAM));
+        return converter;
+    }
+}
---
+++ src/test/java/au/org/aodn/esindexer/EsIndexerApplicationTests.java
@@ -0,0 +1,13 @@
+package au.org.aodn.esindexer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsIndexerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
