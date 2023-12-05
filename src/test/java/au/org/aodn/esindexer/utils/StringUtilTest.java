591ee48d2d76aee3a4f691fc319a9b123ceacc5e
Viet Nguyen <vh.nguyen@utas.edu.au>
2023-12-05 17:23:47 +1100

    Add unit tests for ranking by completeness

--- src/test/java/au/org/aodn/esindexer/utils/StringUtilTest.java
+++ src/test/java/au/org/aodn/esindexer/utils/StringUtilTest.java
@@ -1,4 +1,4 @@
-import au.org.aodn.esindexer.utils.StringUtil;
+package au.org.aodn.esindexer.utils;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
