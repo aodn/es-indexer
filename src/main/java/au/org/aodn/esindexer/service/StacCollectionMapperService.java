4cb027bae7da51d11f67fa3aaabac11121696c39
Viet Nguyen <vh.nguyen@utas.edu.au>
2024-02-28 17:40:22 +1100

    update previous null check

--- src/main/java/au/org/aodn/esindexer/service/StacCollectionMapperService.java
+++ src/main/java/au/org/aodn/esindexer/service/StacCollectionMapperService.java
@@ -190,15 +190,16 @@ public abstract class StacCollectionMapperService {
         List<Map<String,String>> result = new ArrayList<>();
         List<String[]> temp = createExtentTemporal(source);
 
-        Objects.requireNonNull(temp).forEach(t -> {
-            Map<String,String> m = new HashMap<>();
-            m.put("start", t[0]);
-            m.put("end", t[1]);
-
-            result.add(m);
-        });
-
-        return result;
+        if (temp != null) {
+            for (String[] t : temp) {
+                Map<String, String> temporal = new HashMap<>();
+                temporal.put("start", t[0]);
+                temporal.put("end", t[1]);
+                result.add(temporal);
+            }
+            return result;
+        }
+        return null;
     }
 
     @Named("mapSummaries.geometry")
@@ -695,7 +696,7 @@ public abstract class StacCollectionMapperService {
                         List<Object> rawInput = e.getGeographicElement()
                                 .stream()
                                 .map(AbstractEXGeographicExtentPropertyType::getAbstractEXGeographicExtent)
-                                .filter(m -> Objects.requireNonNull(m.getValue() instanceof EXBoundingPolygonType || m.getValue() instanceof EXGeographicBoundingBoxType))
+                                .filter(m -> m != null && (m.getValue() instanceof EXBoundingPolygonType || m.getValue() instanceof EXGeographicBoundingBoxType))
                                 .map(m -> {
                                     if (m.getValue() instanceof EXBoundingPolygonType exBoundingPolygonType) {
                                         if (!exBoundingPolygonType.getPolygon().isEmpty() && exBoundingPolygonType.getPolygon().get(0).getAbstractGeometry() != null) {
