6483c1f43064aad20ecfe4d357e6f4731f998fd3
utas-raymondng <manfai.ng@utas.edu.au>
2024-05-22 17:54:34 +1000

    Add thumbnail

--- indexer/src/test/java/au/org/aodn/esindexer/service/IndexerServiceTests.java
+++ indexer/src/test/java/au/org/aodn/esindexer/service/IndexerServiceTests.java
@@ -53,15 +53,20 @@ public class IndexerServiceTests extends BaseTestClass {
 
     @Test
     public void verifyIsMetadataPublished() throws IOException {
-        insertMetadataRecords("9e5c3031-a026-48b3-a153-a70c2e2b78b9", "classpath:canned/sample1.xml");
-        insertMetadataRecords("830f9a83-ae6b-4260-a82a-24c4851f7119", "classpath:canned/sample2.xml");
+        String uuid1 = "9e5c3031-a026-48b3-a153-a70c2e2b78b9";
+        String uuid2 = "830f9a83-ae6b-4260-a82a-24c4851f7119";
+        try {
+            insertMetadataRecords(uuid1, "classpath:canned/sample1.xml");
+            insertMetadataRecords(uuid2, "classpath:canned/sample2.xml");
 
-        assertTrue("9e5c3031-a026-48b3-a153-a70c2e2b78b9 published", indexerService.isMetadataPublished("9e5c3031-a026-48b3-a153-a70c2e2b78b9"));
-        assertTrue("830f9a83-ae6b-4260-a82a-24c4851f7119 published", indexerService.isMetadataPublished("830f9a83-ae6b-4260-a82a-24c4851f7119"));
-        assertFalse("Not exist and not published", indexerService.isMetadataPublished("not-exist"));
-
-        deleteRecord("9e5c3031-a026-48b3-a153-a70c2e2b78b9");
-        deleteRecord("830f9a83-ae6b-4260-a82a-24c4851f7119");
+            assertTrue(uuid1 + " published", indexerService.isMetadataPublished(uuid1));
+            assertTrue(uuid2 + " published", indexerService.isMetadataPublished(uuid2));
+            assertFalse("Not exist and not published", indexerService.isMetadataPublished("not-exist"));
+        }
+        finally {
+            deleteRecord(uuid1);
+            deleteRecord(uuid2);
+        }
     }
     /**
      * Read the function implementation on why need to insert 1 docs
@@ -69,59 +74,78 @@ public class IndexerServiceTests extends BaseTestClass {
      */
     @Test
     public void verifyGeoNetworkInstanceReinstalled() throws IOException {
-        insertMetadataRecords("9e5c3031-a026-48b3-a153-a70c2e2b78b9", "classpath:canned/sample1.xml");
-        assertTrue("New installed", indexerService.isGeoNetworkInstanceReinstalled(1));
-
-        deleteRecord("9e5c3031-a026-48b3-a153-a70c2e2b78b9");
+        String uuid = "9e5c3031-a026-48b3-a153-a70c2e2b78b9";
+        try {
+            insertMetadataRecords(uuid, "classpath:canned/sample1.xml");
+            assertTrue("New installed", indexerService.isGeoNetworkInstanceReinstalled(1));
+        }
+        finally {
+            deleteRecord(uuid);
+        }
     }
 
     @Test
     public void verifyGetDocumentCount() throws IOException {
-        insertMetadataRecords("830f9a83-ae6b-4260-a82a-24c4851f7119", "classpath:canned/sample2.xml");
-        insertMetadataRecords("9e5c3031-a026-48b3-a153-a70c2e2b78b9", "classpath:canned/sample1.xml");
+        String uuid1 = "830f9a83-ae6b-4260-a82a-24c4851f7119";
+        String uuid2 = "9e5c3031-a026-48b3-a153-a70c2e2b78b9";
+        try {
+            insertMetadataRecords(uuid1, "classpath:canned/sample2.xml");
+            insertMetadataRecords(uuid2, "classpath:canned/sample1.xml");
 
-        indexerService.indexAllMetadataRecordsFromGeoNetwork(true);
+            indexerService.indexAllMetadataRecordsFromGeoNetwork(true);
 
-        // The sample1 geometry have error [1:9695] failed to parse field [summaries.proj:geometry] of type [geo_shape]
-        // ErrorCause: {"type":"illegal_argument_exception","reason":"Polygon self-intersection at lat=57.0 lon=-66.0"}
-        //
-        // So it will not insert correctly and result in 1 doc only
-        assertEquals("Doc count correct", 1L, elasticSearchIndexService.getDocumentsCount(INDEX_NAME));
-
-        deleteRecord("9e5c3031-a026-48b3-a153-a70c2e2b78b9");
-        deleteRecord("830f9a83-ae6b-4260-a82a-24c4851f7119");
+            // The sample1 geometry have error [1:9695] failed to parse field [summaries.proj:geometry] of type [geo_shape]
+            // ErrorCause: {"type":"illegal_argument_exception","reason":"Polygon self-intersection at lat=57.0 lon=-66.0"}
+            //
+            // So it will not insert correctly and result in 1 doc only
+            assertEquals("Doc count correct", 1L, elasticSearchIndexService.getDocumentsCount(INDEX_NAME));
+        }
+        finally {
+            deleteRecord(uuid2);
+            deleteRecord(uuid1);
+        }
     }
 
     @Test
     public void verifyDeleteDocumentByUUID() throws IOException {
-        insertMetadataRecords("830f9a83-ae6b-4260-a82a-24c4851f7119", "classpath:canned/sample2.xml");
-        insertMetadataRecords("06b09398-d3d0-47dc-a54a-a745319fbece", "classpath:canned/sample3.xml");
+        String uuid1 = "830f9a83-ae6b-4260-a82a-24c4851f7119";
+        String uuid2 = "06b09398-d3d0-47dc-a54a-a745319fbece";
+        try {
+            insertMetadataRecords(uuid1, "classpath:canned/sample2.xml");
+            insertMetadataRecords(uuid2, "classpath:canned/sample3.xml");
 
-        indexerService.indexAllMetadataRecordsFromGeoNetwork(true);
-        assertEquals("Doc count correct", 2L, elasticSearchIndexService.getDocumentsCount(INDEX_NAME));
+            indexerService.indexAllMetadataRecordsFromGeoNetwork(true);
+            assertEquals("Doc count correct", 2L, elasticSearchIndexService.getDocumentsCount(INDEX_NAME));
 
-        // Only 2 doc in elastic, if we delete it then should be zero
-        indexerService.deleteDocumentByUUID("830f9a83-ae6b-4260-a82a-24c4851f7119");
-        assertEquals("Doc count correct", 1L, elasticSearchIndexService.getDocumentsCount(INDEX_NAME));
+            // Only 2 doc in elastic, if we delete it then should be zero
+            indexerService.deleteDocumentByUUID(uuid1);
+            assertEquals("Doc count correct", 1L, elasticSearchIndexService.getDocumentsCount(INDEX_NAME));
 
-        deleteRecord("830f9a83-ae6b-4260-a82a-24c4851f7119");
-        deleteRecord("06b09398-d3d0-47dc-a54a-a745319fbece");
+        }
+        finally {
+            deleteRecord(uuid1);
+            deleteRecord(uuid2);
+        }
     }
 
     @Test
     public void verifyGetDocumentByUUID() throws IOException {
-        String expected = readResourceFile("classpath:canned/sample4_stac.json");
+        String uuid = "7709f541-fc0c-4318-b5b9-9053aa474e0e";
+        try {
+            String expected = readResourceFile("classpath:canned/sample4_stac.json");
 
-        insertMetadataRecords("7709f541-fc0c-4318-b5b9-9053aa474e0e", "classpath:canned/sample4.xml");
+            insertMetadataRecords(uuid, "classpath:canned/sample4.xml");
 
-        indexerService.indexAllMetadataRecordsFromGeoNetwork(true);
-        Hit<ObjectNode> objectNodeHit = indexerService.getDocumentByUUID("7709f541-fc0c-4318-b5b9-9053aa474e0e");
+            indexerService.indexAllMetadataRecordsFromGeoNetwork(true);
+            Hit<ObjectNode> objectNodeHit = indexerService.getDocumentByUUID(uuid);
 
-        String test = objectNodeHit.source().toPrettyString();
+            String test = objectNodeHit.source().toPrettyString();
 
-        assertEquals("Stac equals", indexerObjectMapper.readTree(expected), indexerObjectMapper.readTree(test));
-
-        deleteRecord("7709f541-fc0c-4318-b5b9-9053aa474e0e");
+            assertEquals("Stac equals " + uuid, indexerObjectMapper.readTree(expected), indexerObjectMapper.readTree(test));
+        }
+        finally {
+            deleteRecord(uuid);
+        }
     }
     /**
      * Some dataset can provide links to logos, this test is use to verify the logo links added correctly to the
@@ -129,17 +153,43 @@ public class IndexerServiceTests extends BaseTestClass {
      */
     @Test
     public void verifyLogoLinkAddedOnIndex() throws IOException {
-        String expected = readResourceFile("classpath:canned/sample5_stac.json");
+        String uuid = "2852a776-cbfc-4bc8-a126-f3c036814892";
+        try {
+            String expected = readResourceFile("classpath:canned/sample5_stac.json");
 
-        insertMetadataRecords("2852a776-cbfc-4bc8-a126-f3c036814892", "classpath:canned/sample5.xml");
+            insertMetadataRecords(uuid, "classpath:canned/sample5.xml");
 
-        indexerService.indexAllMetadataRecordsFromGeoNetwork(true);
-        Hit<ObjectNode> objectNodeHit = indexerService.getDocumentByUUID("2852a776-cbfc-4bc8-a126-f3c036814892");
+            indexerService.indexAllMetadataRecordsFromGeoNetwork(true);
+            Hit<ObjectNode> objectNodeHit = indexerService.getDocumentByUUID(uuid);
 
-        String test = objectNodeHit.source().toPrettyString();
-        logger.info(test);
-        assertEquals("Stac equals", indexerObjectMapper.readTree(expected), indexerObjectMapper.readTree(test));
+            String test = objectNodeHit.source().toPrettyString();
+            assertEquals("Stac equals " + uuid, indexerObjectMapper.readTree(expected), indexerObjectMapper.readTree(test));
+        }
+        finally {
+            deleteRecord(uuid);
+        }
+    }
+    /**
+     * Verify we can add thumbnail link to STAC if exist
+     * @throws IOException - If file not found
+     */
+    @Test
+    public void verifyThumbnailLinkAddedOnIndex() throws IOException {
+        String uuid = "e18eee85-c6c4-4be2-ac8c-930991cf2534";
+        try {
+            String expected = readResourceFile("classpath:canned/sample6_stac.json");
 
-        deleteRecord("2852a776-cbfc-4bc8-a126-f3c036814892");
+            insertMetadataRecords(uuid, "classpath:canned/sample6.xml");
+
+            indexerService.indexAllMetadataRecordsFromGeoNetwork(true);
+            Hit<ObjectNode> objectNodeHit = indexerService.getDocumentByUUID(uuid);
+
+            String test = objectNodeHit.source().toPrettyString();
+            logger.info(test);
+            assertEquals("Stac equals " + uuid, indexerObjectMapper.readTree(expected), indexerObjectMapper.readTree(test));
+        }
+        finally {
+            deleteRecord(uuid);
+        }
     }
 }
