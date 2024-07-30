cd237e6bd2c6a36a6207cee518b8d2600dd38c58
HuaizhiDai <huaizhi.dai@utas.edu.au>
2024-07-31 09:17:08 +1000

    new common util function

--- indexer/src/main/java/au/org/aodn/esindexer/utils/CommonUtils.java
+++ indexer/src/main/java/au/org/aodn/esindexer/utils/CommonUtils.java
@@ -1,6 +1,7 @@
 package au.org.aodn.esindexer.utils;
 
 import java.util.Optional;
+import java.util.function.BooleanSupplier;
 import java.util.function.Supplier;
 
 public class CommonUtils {
@@ -13,6 +14,28 @@ public class CommonUtils {
                 | IndexOutOfBoundsException
                 | ClassCastException ignored) {
             return Optional.empty();
+        }
+    }
+
+    // alternative function for @Retryable annotation when the class is not a spring bean
+    public static void persevere(BooleanSupplier action) {
+        persevere(10, 1, action);
+    }
+
+
+
+    public static void persevere(int maxRetries, int delaySecond, BooleanSupplier action) {
+
+        for (int i = 0; i < maxRetries; i++) {
+            var isSuccessful = action.getAsBoolean();
+            if (isSuccessful) {
+                return;
+            }
+            try {
+                Thread.sleep(delaySecond * 1000L);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
         }
     }
 }
