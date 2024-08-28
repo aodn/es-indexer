aea46e16a585d4bcc734a9ebaffd2ba469cb9554
Viet Nguyen <vietnguyenswin@outlook.com>
2024-08-28 17:14:08 +1000

    Update indexer test config beans

--- indexer/src/test/java/au/org/aodn/esindexer/configuration/IndexerTestConfig.java
+++ indexer/src/test/java/au/org/aodn/esindexer/configuration/IndexerTestConfig.java
@@ -2,6 +2,7 @@ package au.org.aodn.esindexer.configuration;
 
 import au.org.aodn.ardcvocabs.model.VocabModel;
 import au.org.aodn.ardcvocabs.service.ParameterVocabProcessor;
+import au.org.aodn.ardcvocabs.service.PlatformVocabProcessor;
 import au.org.aodn.esindexer.BaseTestClass;
 import org.mockito.Mockito;
 import org.springframework.context.annotation.Bean;
@@ -24,13 +25,26 @@ public class IndexerTestConfig {
      */
     @Bean
     public ParameterVocabProcessor createParameterVocabProcessor() throws IOException {
-        String json = BaseTestClass.readResourceFile("classpath:canned/aodn_discovery_parameter_vocab.json");
+        String json = BaseTestClass.readResourceFile("classpath:canned/aodn_discovery_parameter_vocabs.json");
         List<VocabModel> parameterVocabs = (new ObjectMapper())
                 .readValue(json, new TypeReference<List<VocabModel>>() {});
 
         ParameterVocabProcessor service = Mockito.mock(ParameterVocabProcessor.class);
         when(service.getParameterVocabs(anyString()))
                 .thenReturn(parameterVocabs);
+
+        return service;
+    }
+
+    @Bean
+    public PlatformVocabProcessor createPlatformVocabProcessor() throws IOException {
+        String json = BaseTestClass.readResourceFile("classpath:canned/aodn_platform_vocabs.json");
+        List<VocabModel> platformVocabs = (new ObjectMapper())
+                .readValue(json, new TypeReference<List<VocabModel>>() {});
+
+        PlatformVocabProcessor service = Mockito.mock(PlatformVocabProcessor.class);
+        when(service.getPlatformVocabs(anyString()))
+                .thenReturn(platformVocabs);
 
         return service;
     }
