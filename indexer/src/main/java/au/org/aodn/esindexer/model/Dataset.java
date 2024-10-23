c8e65e853664d838964cbf28feb4903359c666f6
HuaizhiDai <huaizhi.dai@utas.edu.au>
2024-10-23 11:20:30 +1100

    split dataset by month

--- indexer/src/main/java/au/org/aodn/esindexer/model/Dataset.java
+++ indexer/src/main/java/au/org/aodn/esindexer/model/Dataset.java
@@ -1,8 +1,10 @@
 package au.org.aodn.esindexer.model;
 
+import java.time.YearMonth;
 import java.util.List;
 
 public record Dataset(
         String uuid,
+        YearMonth yearMonth,
         List<DataRecord> data
 ) {}
