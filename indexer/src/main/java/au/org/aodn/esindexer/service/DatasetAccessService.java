4562b1958ea930b97e9cafbf6637a7ea8370400b
HuaizhiDai <huaizhi.dai@utas.edu.au>
2024-10-23 15:30:59 +1100

    data access service update

--- indexer/src/main/java/au/org/aodn/esindexer/service/DatasetAccessService.java
+++ indexer/src/main/java/au/org/aodn/esindexer/service/DatasetAccessService.java
@@ -2,8 +2,11 @@ package au.org.aodn.esindexer.service;
 
 import au.org.aodn.esindexer.model.Dataset;
 
+import java.time.LocalDate;
+
 public interface DatasetAccessService {
-    Dataset getIndexingDatasetBy(String uuid);
+    Dataset getIndexingDatasetBy(String uuid, LocalDate startDate, LocalDate endDate);
+    boolean doesDataExist(String uuid, LocalDate startDate, LocalDate endDate);
     String getServiceUrl();
     void setServiceUrl(String url);
 }
--- indexer/src/main/java/au/org/aodn/esindexer/service/DatasetAccessServiceImpl.java
+++ indexer/src/main/java/au/org/aodn/esindexer/service/DatasetAccessServiceImpl.java
@@ -3,13 +3,14 @@ package au.org.aodn.esindexer.service;
 import au.org.aodn.esindexer.exception.MetadataNotFoundException;
 import au.org.aodn.esindexer.model.DataRecord;
 import au.org.aodn.esindexer.model.Dataset;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.Collections;
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,7 +35,13 @@ public class DatasetAccessServiceImpl implements DatasetAccessService {
 
     private final RestTemplate restTemplate = new RestTemplate();
     @Override
-    public Dataset getIndexingDatasetBy(String uuid) {
+    public Dataset getIndexingDatasetBy(String uuid, LocalDate startDate, LocalDate endDate) {
+
+        // currently, we force to get data in the same month and year to simplify the logic
+        if (startDate.getMonth() != endDate.getMonth() || startDate.getYear() != endDate.getYear()) {
+            throw new IllegalArgumentException("Start date and end date must be in the same month and year");
+        }
+
         try {
             HttpEntity<String> request = getRequestEntity(null, null);
 
@@ -43,6 +50,8 @@ public class DatasetAccessServiceImpl implements DatasetAccessService {
 
             String url = UriComponentsBuilder.fromHttpUrl(getDatasetAccessEndpoint() + "/data/{uuid}")
                     .queryParam("is_to_index", "true")
+                    .queryParam("start_date", startDate)
+                    .queryParam("end_date", endDate)
                     .buildAndExpand(uuid)
                     .toUriString();
 
@@ -54,12 +63,19 @@ public class DatasetAccessServiceImpl implements DatasetAccessService {
                     params
             );
 
-//            ResponseEntity<DataRecord[]> responseEntity = restTemplate.getForEntity(url, DataRecord[].class, params);
 
 
             if (responseEntity.getStatusCode().is2xxSuccessful()) {
-                var records = responseEntity.getBody();
-                return new Dataset(uuid, List.of(records));
+                List<DataRecord> records = new ArrayList<>();
+                if (responseEntity.getBody() != null) {
+                    records = List.of(responseEntity.getBody());
+                }
+                var recordsToIndex = summarizeRecords(records);
+                return new Dataset(
+                        uuid,
+                        YearMonth.of(startDate.getYear(), startDate.getMonth()),
+                        recordsToIndex
+                );
             }
             throw new RuntimeException("Unable to retrieve dataset with UUID: " + uuid );
 
@@ -70,11 +86,61 @@ public class DatasetAccessServiceImpl implements DatasetAccessService {
         }
     }
 
+    @Override
+    public boolean doesDataExist(String uuid, LocalDate startDate, LocalDate endDate) {
+        try {
+            HttpEntity<String> request = getRequestEntity(null, null);
+
+            Map<String, Object> params = new HashMap<>();
+            params.put("uuid", uuid);
+
+            String url = UriComponentsBuilder.fromHttpUrl(getDatasetAccessEndpoint() + "/data/{uuid}/has_data")
+                    .queryParam("start_date", startDate)
+                    .queryParam("end_date", endDate)
+                    .buildAndExpand(uuid)
+                    .toUriString();
+
+            ResponseEntity<Boolean> responseEntity = restTemplate.exchange(
+                    url,
+                    HttpMethod.GET,
+                    request,
+                    Boolean.class,
+                    params
+            );
+
+            return Boolean.TRUE.equals(responseEntity.getBody());
+
+        } catch (HttpClientErrorException.NotFound e) {
+            throw new MetadataNotFoundException("Unable to find dataset with UUID: " + uuid + " in GeoNetwork");
+        } catch (Exception e) {
+            throw new RuntimeException("Exception thrown while retrieving dataset with UUID: " + uuid + e.getMessage(), e);
+        }
+    }
+
+    /**
+     * Summarize the records by counting the number if all the concerned fields are the same
+     * @param records the records to summarize
+     * @return the summarized records
+     */
+    private List<DataRecord> summarizeRecords(List<DataRecord> records) {
+        var summarizedRecords = new ArrayList<DataRecord>();
+        for (var record: records) {
+            if (summarizedRecords.contains(record)) {
+                var existingRecord = summarizedRecords.get(summarizedRecords.indexOf(record));
+                existingRecord.incrementCount();
+            } else {
+                summarizedRecords.add(record);
+            }
+        }
+        return summarizedRecords;
+    }
+
     private String getDatasetAccessEndpoint() {
         return getServiceUrl() + "/api/v1/das/";
     }
 
 
+    // parameters are not in use for now. May be useful in the future so just keep it
     protected HttpEntity<String> getRequestEntity(MediaType accept, String body) {
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(List.of(
