d09f062eafce6bb81e7c7d5b25ddd62374eee63b
HuaizhiDai <huaizhi.dai@utas.edu.au>
2025-03-04 09:02:21 +1100

    change the dataaccessservice interface

--- cloudoptimized/src/main/java/au/org/aodn/cloudoptimized/service/DataAccessService.java
+++ cloudoptimized/src/main/java/au/org/aodn/cloudoptimized/service/DataAccessService.java
@@ -1,13 +1,13 @@
 package au.org.aodn.cloudoptimized.service;
 
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Optional;
-
 import au.org.aodn.cloudoptimized.model.MetadataEntity;
 import au.org.aodn.cloudoptimized.model.MetadataFields;
 import au.org.aodn.cloudoptimized.model.TemporalExtent;
-import au.org.aodn.stac.model.StacItemModel;
+import au.org.aodn.cloudoptimized.model.geojson.FeatureCollectionGeoJson;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 public interface DataAccessService {
 
@@ -17,7 +17,7 @@ public interface DataAccessService {
                 List.of(MetadataFields.TIME, MetadataFields.LONGITUDE, MetadataFields.LATITUDE);
     }
 
-    List<StacItemModel> getIndexingDatasetBy(String uuid, LocalDate startDate, LocalDate endDate, List<MetadataFields> fields);
+    FeatureCollectionGeoJson getIndexingDatasetBy(String uuid, LocalDate startDate, LocalDate endDate, List<MetadataFields> fields);
     List<TemporalExtent> getTemporalExtentOf(String uuid);
     Optional<String> getNotebookLink(String uuid);
     MetadataEntity getMetadataByUuid(String uuid);
