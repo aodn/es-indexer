a09954357883cfe523036aa8873661d3d788a14a
HuaizhiDai <huaizhi.dai@utas.edu.au>
2025-03-17 22:32:58 +1100

    use multi thread to speed up

--- cloudoptimized/src/main/java/au/org/aodn/cloudoptimized/model/DatasetProvider.java
+++ cloudoptimized/src/main/java/au/org/aodn/cloudoptimized/model/DatasetProvider.java
@@ -4,13 +4,12 @@ import au.org.aodn.cloudoptimized.model.geojson.FeatureCollectionGeoJson;
 import au.org.aodn.cloudoptimized.service.DataAccessService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.util.StopWatch;
 
+import java.lang.management.ManagementFactory;
 import java.time.LocalDate;
 import java.time.YearMonth;
-import java.util.Iterator;
-import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.*;
+import java.util.concurrent.*;
 
 public class DatasetProvider {
 
@@ -19,48 +18,129 @@ public class DatasetProvider {
     protected YearMonth currentYearMonth;
     protected final YearMonth endYearMonth;
     protected final DataAccessService dataAccessService;
+    protected final ConcurrentHashMap<YearMonth, FeatureCollectionGeoJson> featureCollectionCache = new ConcurrentHashMap<>();
+    protected final List<MetadataFields> columns;
+    private boolean isGettingData = false;
 
-    public DatasetProvider(String uuid, LocalDate startDate, LocalDate endDate, DataAccessService dataAccessService) {
+
+    private final ExecutorService executorService =
+            Executors.newSingleThreadExecutor();
+
+
+    public DatasetProvider(
+            String uuid,
+            LocalDate startDate,
+            LocalDate endDate,
+            DataAccessService dataAccessService,
+            List<MetadataFields> columns
+    ) {
         this.uuid = uuid;
         this.dataAccessService = dataAccessService;
         this.currentYearMonth = YearMonth.from(startDate);
         this.endYearMonth = YearMonth.from(endDate);
+        this.columns = columns;
+        executorService.submit(this::initializeCache);
     }
 
-    public Iterable<FeatureCollectionGeoJson> getIterator(List<MetadataFields> columns) {
+    private record FeatureCollectionTask(YearMonth yearMonth, FeatureCollectionGeoJson featureCollection){};
+
+    private void initializeCache() {
+        try {
+            while (!isCacheFull() && !currentYearMonth.isAfter(endYearMonth)) {
+                fetchData50Threads();
+                System.out.println();
+            }
+        } finally {
+            executorService.shutdown();
+        }
+    }
+
+    private void fetchData50Threads() {
+        final int THREADS_COUNT = 15;
+        isGettingData = true;
+        ExecutorService executorService = Executors.newFixedThreadPool(THREADS_COUNT);
+        List<Callable<FeatureCollectionTask>> tasks = new ArrayList<>();
+        for (int i = 0; i < THREADS_COUNT; i++) {
+            var taskYearMonth = YearMonth.from(currentYearMonth);
+            tasks.add(() -> getFeatureCollection(columns, taskYearMonth));
+            currentYearMonth = currentYearMonth.plusMonths(1);
+            if (currentYearMonth.isAfter(endYearMonth)) {
+                break;
+            }
+        }
+
+        try {
+            List<Future<FeatureCollectionTask>> futures = executorService.invokeAll(tasks);
+            for (Future<FeatureCollectionTask> future : futures) {
+                FeatureCollectionTask featureCollectionTask = future.get();
+                if (featureCollectionTask != null && featureCollectionTask.featureCollection != null) {
+                    featureCollectionCache.put(featureCollectionTask.yearMonth, featureCollectionTask.featureCollection);
+                }
+            }
+            isGettingData = false;
+        } catch (InterruptedException | ExecutionException e) {
+            log.error("Error while fetching data", e);
+        } finally {
+            executorService.shutdown();
+        }
+    }
+
+
+    private boolean isCacheFull() {
+        long maxHeap = ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getMax();
+        long usedHeap = ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getUsed();
+        double usedPercentage = (double) usedHeap / maxHeap * 100;
+
+        log.info("Used heap: {}% of max heap", usedPercentage);
+
+        // to make sure the app is safe, set a lower percentage
+        return usedPercentage >= 50;
+    }
+
+    public Iterable<FeatureCollectionGeoJson> getIterator() {
+
         return () -> new Iterator<>() {
             @Override
             public boolean hasNext() {
-                return !currentYearMonth.isAfter(endYearMonth);
+                return currentYearMonth.isBefore(endYearMonth) || isGettingData;
             }
 
             @Override
             public FeatureCollectionGeoJson next() {
-                // please keep it for a while since it benefits the performance optimisation
-                StopWatch timer = new StopWatch();
-                timer.start(String.format("Data querying for %s %s", currentYearMonth.getYear(), currentYearMonth.getMonth()));
                 if (!hasNext()) {
                     throw new NoSuchElementException();
                 }
-
-                FeatureCollectionGeoJson featureCollection = dataAccessService.getIndexingDatasetBy(
-                        uuid,
-                        LocalDate.of(currentYearMonth.getYear(), currentYearMonth.getMonthValue(), 1),
-                        LocalDate.of(currentYearMonth.getYear(), currentYearMonth.getMonthValue(), currentYearMonth.lengthOfMonth()),
-                        columns
-                );
-                currentYearMonth = currentYearMonth.plusMonths(1);
-                if (featureCollection == null
-                        || featureCollection.getFeatures() == null
-                        || featureCollection.getFeatures().isEmpty()
-                ) {
-                    return null;
+                while (getEarliestYearMonth() == null) {
+                    try{
+                        // TODO: use synchronized block , wait() and notify() to avoid busy waiting
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        log.error("Error while waiting for data", e);
+                        Thread.currentThread().interrupt();
+                        throw new RuntimeException(e);
+                    }
                 }
-
-                timer.stop();
-                log.info(timer.prettyPrint());
-                return featureCollection;
+                return featureCollectionCache.remove(getEarliestYearMonth());
             }
         };
+    }
+
+    private FeatureCollectionTask getFeatureCollection(List<MetadataFields> columns, YearMonth yearMonth) {
+        var startDate = yearMonth.atDay(1);
+        var endDate = yearMonth.atEndOfMonth();
+        var featureCollection =  dataAccessService.getIndexingDatasetBy(
+                uuid,startDate,
+                endDate,
+                columns
+        );
+        return new FeatureCollectionTask(yearMonth, featureCollection);
+    }
+
+    private YearMonth getEarliestYearMonth() {
+        return featureCollectionCache.entrySet()
+                .stream()
+                .min(Map.Entry.comparingByKey())
+                .map(Map.Entry::getKey)
+                .orElse(null);
     }
 }
