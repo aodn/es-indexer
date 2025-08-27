d0bda5bea28dc70540b7e18f42ba34ad484c6abf
rng <manfai.ng@utas.edu.au>
2025-08-27 17:07:51 +1000

    Add function to run report

---
+++ indexer/src/main/java/au/org/aodn/esindexer/controller/IndexerReportController.java
@@ -0,0 +1,73 @@
+package au.org.aodn.esindexer.controller;
+
+import au.org.aodn.esindexer.service.ElasticSearchIndexService;
+import au.org.aodn.esindexer.utils.CommonUtils;
+import au.org.aodn.esindexer.utils.JaxbUtils;
+import au.org.aodn.metadata.geonetwork.service.GeoNetworkService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import au.org.aodn.metadata.iso19115_3_2018.MDMetadataType;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+@RestController
+@RequestMapping(value = "/api/v1/indexer/report/")
+@Tag(name="Indexer Report", description = "Generate report for troubleshoot")
+@Slf4j
+public class IndexerReportController {
+    protected ElasticSearchIndexService elasticSearchIndexService;
+    protected GeoNetworkService geoNetworkResourceService;
+    protected JaxbUtils<MDMetadataType> jaxbUtils;
+    protected String indexName;
+
+    @Autowired
+    public IndexerReportController(
+            @Value("${elasticsearch.index.name}") String indexName,
+            JaxbUtils<MDMetadataType> jaxbUtils,
+            ElasticSearchIndexService elasticSearchIndexService,
+            GeoNetworkService geoNetworkResourceService) {
+
+        this.indexName = indexName;
+        this.jaxbUtils = jaxbUtils;
+        this.elasticSearchIndexService = elasticSearchIndexService;
+        this.geoNetworkResourceService = geoNetworkResourceService;
+    }
+
+    @GetMapping(path="/uuid-mismatch")
+    public ResponseEntity<List<String>> findUUidNotInElastic() {
+        List<String> uuids = new ArrayList<>();
+        Iterable<String> records = this.geoNetworkResourceService.getAllMetadataRecords(null);
+        Iterator<String> i = records.iterator();
+
+        CountDownLatch cd = new CountDownLatch(1);
+        while(i.hasNext()) {
+            String r = i.next();
+            try {
+                if (r != null) {
+                    MDMetadataType doc = jaxbUtils.unmarshal(r);
+                    String uuid = CommonUtils.getUUID(doc);
+
+                    log.info("Comparing uuid {}", uuid);
+                    if (elasticSearchIndexService.getFirstMatchId(this.indexName, "id", uuid) == null) {
+                        uuids.add(uuid);
+                    }
+                    cd.await(50, TimeUnit.MILLISECONDS);
+                }
+            } catch (Exception e) {
+                uuids.add(String.format("%s[%s]", e.getMessage(), r));
+            }
+        }
+
+        return ResponseEntity.ok().body(uuids);
+    }
+
+}
