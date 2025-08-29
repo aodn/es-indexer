45651d3c2bbc198ee9982a3ede905eac52068aae
HuaizhiDai <huaizhi.dai@utas.edu.au>
2025-08-29 11:27:25 +1000

    add batch option in entry point

--- indexer/src/main/java/au/org/aodn/esindexer/Application.java
+++ indexer/src/main/java/au/org/aodn/esindexer/Application.java
@@ -1,7 +1,11 @@
 package au.org.aodn.esindexer;
 
+import au.org.aodn.esindexer.batch.BatchJobRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
@@ -13,8 +17,38 @@ public class Application {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
 
+    private static final String BATCH = "batch";
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
 
+        // if the first argument is not "batch", start the web application as a backend server
+        if (args.length == 0 || !args[0].equals(BATCH)) {
+            SpringApplication.run(Application.class, args);
+            return;
+        }
+
+
+        // otherwise, run the batch job
+        if (args.length != 2) {
+            System.err.println("Argument count mismatch. Arg count: " + args.length);
+            System.exit(1);
+        }
+
+        ConfigurableApplicationContext context = new SpringApplicationBuilder(Application.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+        BatchJobRunner runner = context.getBean(BatchJobRunner.class);
+
+
+            String jobName = args[1];
+            try {
+                runner.run(jobName);
+            } catch (Exception e) {
+                System.err.println("Batch job failed with exception: " + e.getMessage());
+                System.exit(1);
+            }
+            System.out.println("Batch job completed successfully.");
+            System.exit(0);
+
+
+    }
 }
