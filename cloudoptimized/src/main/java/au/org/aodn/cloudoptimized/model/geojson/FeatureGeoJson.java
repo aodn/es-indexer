492541c003f4e4d948917b5eedca2f91dd13bd5d
HuaizhiDai <huaizhi.dai@utas.edu.au>
2025-10-07 21:59:18 +1100

    enhance test case

--- cloudoptimized/src/main/java/au/org/aodn/cloudoptimized/model/geojson/FeatureGeoJson.java
+++ cloudoptimized/src/main/java/au/org/aodn/cloudoptimized/model/geojson/FeatureGeoJson.java
@@ -1,5 +1,9 @@
 package au.org.aodn.cloudoptimized.model.geojson;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.util.HashMap;
@@ -17,6 +21,17 @@ public class FeatureGeoJson {
         this.geometry = geometry;
         this.properties = new HashMap<>();
     }
+
+    @JsonCreator
+    public FeatureGeoJson(
+            @JsonProperty("type") String type,
+            @JsonProperty("geometry") GeometryGeoJson geometry,
+            @JsonProperty("properties") Map<String, Object> properties) {
+        this.type = type;
+        this.geometry = geometry;
+        this.properties = properties != null ? properties : new HashMap<>();
+    }
+
 
     public void addProperty(String key, Object value) {
         properties.put(key, value);
--- cloudoptimized/src/test/java/au/org/aodn/cloudoptimized/service/DataAccessServiceImplTest.java
+++ cloudoptimized/src/test/java/au/org/aodn/cloudoptimized/service/DataAccessServiceImplTest.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.time.YearMonth;
 import java.util.ArrayList;
@@ -30,43 +31,97 @@ class DataAccessServiceImplTest {
     public void setUp() {
         mockRestTemplate = mock(RestTemplate.class);
 
+
+    }
+
+    @Test
+    void testGetZarrIndexingDataByMonth() {
+        // Mock WebClient components
+        WebClient.RequestHeadersUriSpec requestHeadersUriSpec = mock(WebClient.RequestHeadersUriSpec.class);
+        WebClient.RequestHeadersSpec requestHeadersSpec = mock(WebClient.RequestHeadersSpec.class);
+        WebClient.ResponseSpec responseSpec = mock(WebClient.ResponseSpec.class);
+
+        // Define the mock response
+        String mockJsonResponse = """
+        {
+                         "type": "FeatureCollection",
+                         "features": [
+                             {
+                                 "type": "Feature",
+                                 "geometry": {
+                                     "type": "Polygon",
+                                     "coordinates": [
+                                         [
+                                             [
+                                                 132.953957,
+                                                 -37.454966
+                                             ],
+                                             [
+                                                 132.953957,
+                                                 -34.823425
+                                             ],
+                                             [
+                                                 137.443624,
+                                                 -34.823425
+                                             ],
+                                             [
+                                                 137.443624,
+                                                 -37.454966
+                                             ],
+                                             [
+                                                 132.953957,
+                                                 -37.454966
+                                             ]
+                                         ]
+                                     ]
+                                 },
+                                 "properties": {
+                                     "date": "2011-05",
+                                     "count": 3849480
+                                 }
+                             }
+                         ],
+                         "properties": {
+                             "date": "2011-05",
+                             "collection": "db049981-3d4e-4cb2-9c4b-e697650845b9",
+                             "key": "radar_SouthAustraliaGulfs_wind_delayed_qc.zarr"
+                         }
+                     }
+    """;
+
+        // Mock the WebClient behavior
+        WebClient mockWebClient = mock(WebClient.class);
+        when(mockWebClient.get()).thenReturn(requestHeadersUriSpec);
+        when(requestHeadersUriSpec.uri(anyString())).thenReturn(requestHeadersSpec);
+        when(requestHeadersSpec.accept(any())).thenReturn(requestHeadersSpec);
+        when(requestHeadersSpec.retrieve()).thenReturn(responseSpec);
+        when(responseSpec.bodyToMono(String.class)).thenReturn(Mono.just(mockJsonResponse));
+
+        // Inject the mocked WebClient into the service
         dataAccessService = new DataAccessServiceImpl(
                 "server-url",
                 "base-url",
                 mockRestTemplate,
-                mock(WebClient.class),
+                mockWebClient,
                 new ObjectMapper()
         );
-    }
 
-    @Test
-    void getZarrIndexingDataByMonthTest() {
-        var polygon = new PolygonGeoJson(new ArrayList< List<List<Double>>>());
-        var feature = new FeatureGeoJson(polygon);
-        var features = List.of(feature);
-        var mockFeatureCollection = new FeatureCollectionGeoJson();
-        mockFeatureCollection.setFeatures(features);
-        var mockResponse = new ResponseEntity<>(mockFeatureCollection, HttpStatus.OK);
-
-
-        when(mockRestTemplate.exchange(
-                anyString(),
-                any(),
-                any(),
-                any(ParameterizedTypeReference.class),
-                anyMap()
-        )).thenReturn(mockResponse);
-
-        DataAccessServiceImpl service = Mockito.mock(DataAccessServiceImpl.class);
-        String testMonth = "2024-06";
-        List<String> expected = Collections.singletonList("test-data");
-
+        // Call the method under test
         var result = dataAccessService.getZarrIndexingDataByMonth("test-uuid", "test-key", YearMonth.of(2024, 6));
 
-
-        // Assert
+        // Assert the result
         assertNotNull(result);
-        assertEquals(mockFeatureCollection, result);
-
+        assertEquals(1, result.getFeatures().size());
+        FeatureGeoJson feature = result.getFeatures().get(0);
+        assertEquals("Feature", feature.getType());
+        assertTrue(feature.getGeometry() instanceof PolygonGeoJson);
+        PolygonGeoJson polygon = (PolygonGeoJson) feature.getGeometry();
+        assertEquals(5, polygon.getCoordinates().get(0).size()); // 5 coordinates in the polygon
+        assertEquals("2011-05", feature.getProperties().get("date"));
+        assertEquals(3849480, feature.getProperties().get("count"));
+        assertEquals("2011-05", result.getProperties().get("date"));
+        assertEquals("db049981-3d4e-4cb2-9c4b-e697650845b9", result.getProperties().get("collection"));
+        assertEquals("radar_SouthAustraliaGulfs_wind_delayed_qc.zarr", result.getProperties().get("key"));
     }
+
 }
