f4bdf9d888db16cd9515d31502c97a1a224f9a35
Lyn Long <linlin.long@utas.edu.au>
2026-05-18 16:55:40 +1000

    update ContactsAddressModel

--- stacmodel/src/main/java/au/org/aodn/stac/model/ContactsAddressModel.java
+++ stacmodel/src/main/java/au/org/aodn/stac/model/ContactsAddressModel.java
@@ -2,6 +2,7 @@ package au.org.aodn.stac.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -12,10 +13,13 @@ import java.util.Objects;
 @Builder
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class ContactsAddressModel {
+    @JsonProperty("delivery_point")
     protected List<String> deliveryPoint;
     protected String city;
     protected String country;
+    @JsonProperty("postal_code")
     protected String postalCode;
+    @JsonProperty("administrative_area")
     protected String administrativeArea;
 
     @JsonIgnore
